3e7bf844a2e600a82b39e42ef1798104f87f0d72
jorge perez <javierjorge77@gmail.com>
2022-11-26 19:45:58 -0600

    active admin configurado como MVP

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_024821) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_27_000050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+  end
 
   create_table "consultations", force: :cascade do |t|
     t.string "rut"
@@ -70,10 +84,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_024821) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.datetime "creation_date"
-    t.integer "num_consultation"
-    t.datetime "date_consultation"
     t.boolean "contract"
-    t.integer "total_consultations"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
